bc955bdd6de26724639f9dc683e90034c1547668
Hrishikesh778 <hrishi778@gmail.com>
2024-11-26 16:55:26 +0530

    first java script

---
+++ first.js
@@ -0,0 +1,66 @@
+
+// a = 7
+
+// console.log(a*1)
+// console.log(a*2)
+// console.log(a*3)
+// console.log(a*4)
+// console.log(a*5)
+// console.log(a*6)
+// console.log(a*7)
+// console.log(a*8)
+// console.log(a*8)
+// console.log(a*10)
+
+
+// fname = 'Atiq'
+// lname = "ansari"
+// mName = `husain`
+// console.log(fname)
+// console.log(mName)
+// console.log(lname)
+
+
+// console.log("first")
+
+// isloggedIn = true
+// isloggedIn = false
+
+// console.log(isloggedIn)
+
+// mNumber = BigInt(9876543456789876908765678909876545678765678976567897657897657897678976789767897656789767897657897678978908765)
+// console.log(mNumber)
+
+jsBatch = ['a','b','c','d']
+console.log(jsBatch)
+console.log(jsBatch[0])
+console.log(jsBatch[1])
+console.log(jsBatch[2])
+console.log(jsBatch[3])
+console.log(jsBatch[4])
+
+
+
+
+htmlBatch = []
+htmlBatch[0] = 'yadnesh'
+htmlBatch[1] = 'Hrishikesh'
+console.log(htmlBatch)
+
+jsBatchObj = {
+    fname : "Ria",
+    lname : "gupta",
+    11 : 11
+}
+
+console.log(jsBatchObj.fname)
+console.log(jsBatchObj.lname)
+console.log(jsBatchObj.age)
+console.log(jsBatchObj[11])
+
+// fname = "krishna"
+mNumber = 54345
+console.log(typeof mNumber)
+jj = false
+console.log(typeof jj, "sfhdbvh")
+console.log(Array.isArray(jsBatchObj))
